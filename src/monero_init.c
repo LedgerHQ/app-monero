65dc0690197f5264b0cb9d1fdc646a9293fcedfa
cslashm <cslashm@gmail.com>
2019-07-23 18:26:25 +0200

    add v0.14.1.2 support (bis)

--- src/monero_init.c
+++ src/monero_init.c
@@ -48,9 +48,9 @@ void monero_init() {
   G_monero_vstate.state = STATE_IDLE;
 }
 
-#define MONERO_SUPPORTED_CLIENT_SIZE 1
+#define MONERO_SUPPORTED_CLIENT_SIZE 2
 const char * const monero_supported_client[MONERO_SUPPORTED_CLIENT_SIZE] = {
-  "0.14.1.0","0.14.1.2",
+  "0.14.1.0", "0.14.1.2",
 };
 
 int monero_apdu_reset() {
