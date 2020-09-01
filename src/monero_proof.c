c1ec34dcba28c1e65636885df3ebb8c3cdc05200
François Colas <francois.colas@ledger.fr>
2020-09-01 16:08:25 +0200

    Update INS_GET_TX_PROOF to InProofv2

--- src/monero_proof.c
+++ src/monero_proof.c
@@ -25,16 +25,6 @@
 /* ----------------------------------------------------------------------- */
 /* ---                                                                 --- */
 /* ----------------------------------------------------------------------- */
-/*
- * pick random k
- * if B:
- *   compute X = k*B
- * else:
- *   compute X = k*G
- * compute Y = k*A
- * sig.c = Hs(Msg || D || X || Y)
- * sig.r = k - sig.c*r
- */
 int monero_apdu_get_tx_proof() {
     unsigned char *msg;
     unsigned char *R;
@@ -45,6 +35,7 @@ int monero_apdu_get_tx_proof() {
     unsigned char XY[32];
     unsigned char sig_c[32];
     unsigned char sig_r[32];
+    unsigned char sep[32];
 #define k (G_monero_vstate.tmp + 256)
 
     msg = G_monero_vstate.io_buffer + G_monero_vstate.io_offset;
@@ -61,23 +52,43 @@ int monero_apdu_get_tx_proof() {
 
     monero_io_discard(0);
 
+    // Generate random k
     monero_rng_mod_order(k);
+    // tmp = msg
     os_memmove(G_monero_vstate.tmp + 32 * 0, msg, 32);
+    // tmp = msg || D
     os_memmove(G_monero_vstate.tmp + 32 * 1, D, 32);
 
     if (G_monero_vstate.options & 1) {
+        // X = kB
         monero_ecmul_k(XY, B, k);
     } else {
+        // X = kG
         monero_ecmul_G(XY, k);
     }
+    // tmp = msg || D || X
     os_memmove(G_monero_vstate.tmp + 32 * 2, XY, 32);
 
+    // Y = kA
     monero_ecmul_k(XY, A, k);
+    // tmp = msg || D || X || Y
     os_memmove(G_monero_vstate.tmp + 32 * 3, XY, 32);
+    monero_keccak_H("TXPROOF_V2", 10, sep);
+    // tmp = msg || D || X || Y || sep
+    os_memmove(G_monero_vstate.tmp + 32 * 4, sep, 32);
+    // tmp = msg || D || X || Y || sep || R
+    os_memmove(G_monero_vstate.tmp + 32 * 5, R, 32);
+    // tmp = msg || D || X || Y || sep || R || A
+    os_memmove(G_monero_vstate.tmp + 32 * 6, A, 32);
+    // tmp = msg || D || X || Y || sep || R || B or [0]
+    os_memmove(G_monero_vstate.tmp + 32 * 7, B, 32);
 
-    monero_hash_to_scalar(sig_c, &G_monero_vstate.tmp[0], 32 * 4);
+    // sig_c = H_n(tmp)
+    monero_hash_to_scalar(sig_c, &G_monero_vstate.tmp[0], 32 * 8);
 
+    // sig_c*r
     monero_multm(XY, sig_c, r);
+    // sig_r = k - sig_c*r
     monero_subm(sig_r, k, XY);
 
     monero_io_insert(sig_c, 32);
