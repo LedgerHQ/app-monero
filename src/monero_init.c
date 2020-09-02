9e2f8eb8c884cff155e97f003e437604fd114c62
François Colas <francois.colas@ledger.fr>
2020-09-02 18:03:02 +0200

    Add next minimum version for Monero client

--- src/monero_init.c
+++ src/monero_init.c
@@ -179,7 +179,7 @@ void monero_install(unsigned char netId) {
 /* ----------------------------------------------------------------------- */
 #define MONERO_SUPPORTED_CLIENT_SIZE 1
 const char* const monero_supported_client[MONERO_SUPPORTED_CLIENT_SIZE] = {
-    "0.16.0.",
+    "0.17.0.",
 };
 
 int monero_apdu_reset() {
