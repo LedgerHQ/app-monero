6878abc9995b49ede07c829eeda175df8daf2f45
François Colas <francois.colas@ledger.fr>
2020-09-02 18:03:37 +0200

    Update unit tests with new versions

--- tests/test_version.py
+++ tests/test_version.py
@@ -6,10 +6,10 @@ from monero_client.exception import *
 
 def test_version(monero):
     major, minor, patch = monero.reset_and_get_version(
-        monero_client_version=b"0.16.0.0"
+        monero_client_version=b"0.17.0.0"
     )  # type: int, int, int
 
-    assert (major, minor, patch) == (1, 6, 0)  # version of the Monero app
+    assert (major, minor, patch) == (1, 7, 0)  # version of the Monero app
 
 
 @pytest.mark.xfail(raises=ClientNotSupported)
