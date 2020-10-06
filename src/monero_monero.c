9553396410d1d41dfe334125bff0f06b30fa66ee
François Colas <francois.colas@ledger.fr>
2020-10-06 15:11:43 +0200

    Fix bad display of base58 address

--- src/monero_monero.c
+++ src/monero_monero.c
@@ -64,6 +64,8 @@ const unsigned int encoded_block_sizes[] = {0, 2, 3, 5, 6, 7, 9, 10, 11};
 #define FULL_BLOCK_SIZE         8  //(sizeof(encoded_block_sizes) / sizeof(encoded_block_sizes[0]) - 1)
 #define FULL_ENCODED_BLOCK_SIZE 11  // encoded_block_sizes[full_block_size];
 #define ADDR_CHECKSUM_SIZE      4
+#define ADDR_LEN                95
+#define INTEGRATED_ADDR_LEN     106
 
 static uint64_t uint_8be_to_64(const unsigned char* data, size_t size) {
     uint64_t res = 0;
@@ -168,6 +170,12 @@ int monero_base58_public_key(char* str_b58, unsigned char* view, unsigned char* 
     if (0 < last_block_size) {
         encode_block(data + full_block_count * FULL_BLOCK_SIZE, last_block_size,
                      &str_b58[full_block_count * FULL_ENCODED_BLOCK_SIZE]);
+    }
+
+    if (paymanetID) {
+        str_b58[INTEGRATED_ADDR_LEN] = '\0';
+    } else {
+        str_b58[ADDR_LEN] = '\0';
     }
 
     return 0;
