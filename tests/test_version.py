ee1500fbebbae4d19181e90a3b21aa5ebcf481b7
tdejoigny-ledger <thibault.de-joigny@ledger.fr>
2024-05-30 17:36:42 +0200

    fix PR review remarks

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,3 +1,5 @@
+from typing import List
+import re
 import pytest
 
 # pylint: disable=wildcard-import, unused-wildcard-import
@@ -8,7 +10,7 @@ def check_accepted_version(monero, valid_version: bytes):
     major, minor, patch = monero.reset_and_get_version(
         monero_client_version=valid_version
     )  # type: int, int, int
-    assert (major, minor, patch) == (2, 0, 0)  # version of the Monero app
+    _verify_version(f"{major}.{minor}.{patch}")
 
 
 def check_refused_version(monero, invalid_version: bytes):
@@ -25,3 +27,33 @@ def test_version(monero):
     check_accepted_version(monero, b"0.18.9.0")
     check_accepted_version(monero, b"0.18.18.0")
 
+
+def _verify_version(version: str) -> None:
+    """Verify the app version, based on defines in Makefile
+
+    Args:
+        Version (str): Version to be checked
+    """
+
+    vers_dict = {}
+    vers_str = ""
+    lines = _read_makefile()
+    version_re = re.compile(r"^APPVERSION_(?P<part>\w)\s?=\s?(?P<val>\d*)", re.I)
+    for line in lines:
+        info = version_re.match(line)
+        if info:
+            dinfo = info.groupdict()
+            vers_dict[dinfo["part"]] = dinfo["val"]
+    try:
+        vers_str = f"{vers_dict['M']}.{vers_dict['N']}.{vers_dict['P']}"
+    except KeyError:
+        pass
+    assert version == vers_str
+
+
+def _read_makefile() -> List[str]:
+    """Read lines from the parent Makefile """
+
+    with open("Makefile", "r", encoding="utf-8") as f_p:
+        lines = f_p.readlines()
+    return lines
